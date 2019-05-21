cab96d37fe0f21a7c7da4dd934ff05aa5a89ee0c
Sally Haefling <sallyhaefling@gmail.com>
2019-05-21 17:23:37 -0600

    Add get
    Co-authored-by: Justin Pyktel <justinpyktel@gmail.com>
    Co-authored-by: Lauren Boyer <lboyer4@outlook.com>

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
+app.locals.cardList = [{ id: 0, title: 'test', content: 'lorem ipsum' }]
 
 app.set('port', process.send.PORT || 3000)
 app.locals.title = 'Sticky-Dos'
+app.get('/', (request, response) => response.send('Oh hey there'))
+
+app.get('/api/v1/cardList', (request, response) => {
+  const cardList = app.locals.cardList
+    return response.json({ cardList })
+})
+
+app.use(express.json())
 app.listen(app.get('port'), () => console.log(`${app.locals.title} is running on localhost:${app.get('port')}`))
