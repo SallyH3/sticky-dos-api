8bf65bcbc3596afc68e63aeb8a7eeb235bdf6d8b
Sally Haefling <sallyhaefling@gmail.com>
2019-05-22 16:43:13 -0600

    Add test scaffold
    Co-authored-by: Justin Pyktel <justinpyktel@gmail.com>

--- app.spec.js
+++ app.spec.js
@@ -3,9 +3,9 @@ const babel = require('@babel/polyfill');
 const app =require('./app.js');
 
 describe('API', () => {
-  let mockCardList;
+  let cardList;
   beforeEach(() => {
-    mockCardList = [
+    cardList = [
       { 
         id: 0, 
         title: 'test', 
@@ -18,20 +18,10 @@ describe('API', () => {
           }
         ]
       },
-      {
-        id: 1, 
-        title: 'test2', 
-        content: [
-          {
-            type: 'list',
-            text: 'sample list',
-            checked: true
-          }
-        ]
-      }
     ]
-    app.locals.cardList = mockCardList
+    app.locals.cardList = cardList
   })
+
   describe('GET /api/v1/cardList', () => {
     it('should have a status of 200', async() => {
       const response = await request(app).get('/api/v1/cardList')
@@ -39,14 +29,22 @@ describe('API', () => {
       expect(response.statusCode).toBe(200)
     })
 
-    it('should return an array of cards', async() => {
-
+    it.skip('should return an array of cards', async() => {
+      const response = await request(app).get('/api/v1/cardList')
+      expect(response.body).toEqual(cardList)
     })
 
     it('should return a status of 404 if card is not found', async() => {
       const response = await request(app).get('/api/v1/cardLis')
 
       expect(response.statusCode).toBe(404)
+    })
+
+    it.skip('should respond with the correct card if it exists', async() => {
+      const response = await request(app).get('/api/v1/cardList/')
+      const expected = cardList
+      // console.log(response.body)
+      expect(response.body).toEqual(expected)
     })
   })
 })
