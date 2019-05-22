5860a7cda13222594f5fe27d87fc492a7f3827e9
Sally Haefling <sallyhaefling@gmail.com>
2019-05-21 18:04:17 -0600

    Add cors to backend
    Co-authored-by: Justin Pyktel <justinpyktel@gmail.com>
    Co-authored-by: Lauren Boyer <lboyer4@outlook.com>

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express')
 const app = express()
+// app.use(cors())
+// const cors = require('cors');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+ 
+ app.get('/', function(req, res, next) {
+  // Handle the get for this route
+ });
+ 
+ app.post('/', function(req, res, next) {
+ // Handle the post for this route
+ });
+
 app.locals.cardList = [
   { 
     id: 0, 
