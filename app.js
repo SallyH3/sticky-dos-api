77c8ce7d1b070805be980ed118d636b49458b163
Lauren Boyer <lboyer4@outlook.com>
2019-06-23 09:53:16 -0600

    Added annotation to the app.js file

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 
+//declares express() as the variable app
 const app = express();
+//allows the server to read JSON requests
 app.use(express.json());
+//allows cross origin resource sharing
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
- });
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+//  });
  
+ //cards we keep save in the backend to provide the user info on how to use the site
  app.locals.cardList = [
   { 
     id: 0, 
@@ -44,60 +48,77 @@ app.use(function(req, res, next) {
   }
 ]
 
- //these below are for home path
+ //handles the get for the home path
  app.get('/', function(req, res, next) {
-  // Handle the get for this route
  });
  
+ //handles the post for the home path
  app.post('/', function(req, res, next) {
- // Handle the post for this route
  });
 
- //this below is for post of data
+ //handles the post for the cardlist path
  app.post('/api/v1/cardList', (request, response) => {
+//sets the body of the request as the cardList
   const cardList = request.body;
 
+//if there is not a cardList it returns an error of 422
   if (!cardList) {
     return response.status(422).send({
       error: request.body
     });
   } else {
+//if there is a cardList, add it to the main cardList and return a 201 status as well as the whole cardList
     app.locals.cardList.push({...cardList});
     return response.status(201).json({...cardList});
   }
 })
 
-// app.set('port', process.send.PORT || 3000)
+//sets 'Stick-Dos' as the title
 app.locals.title = 'Sticky-Dos'
-app.get('/', (request, response) => response.send('Oh hey there'))
 
+//returns oh hey there if a response is successful
+// app.get('/', (request, response) => response.send('Oh hey there'))
+
+//handles a get request from the cardlist path
 app.get('/api/v1/cardList', (request, response) => {
+//sets the cardlist as the variable
   const cardList = app.locals.cardList
+//if there is a cardList return it
   if(app.locals.cardList) {
     return response.json({ cardList })
   } else {
+//if there is not a cardList return an error
     response.status(404).send({
       error: request.body
     })
   }
 })
 
+//handles the delete card from the path of that specific card
 app.delete('/api/v1/cardList/:id', (request, response) => {
+//finds the card in the cardlist with the matching id
   const cardIndex = app.locals.cardList.findIndex(card => card.id == request.params.id)
-
+//if no card is found that matches it return an error
   if( cardIndex == -1 ) return response.status(404).json('card not found');
-  
+//if card matches, it is spliced out and returns a 204 status
   app.locals.cardList.splice(cardIndex, 1);
     return response.sendStatus(204);
 });
 
+//handles an edit/put of a specific card
 app.put('/api/v1/cardList/:id', (request, res) => {
+//declares the request body as the title and content
   const { title, content } = request.body;
+//lets the id be the identifier
   let { id } = request.params;
+//declares the cardlist to be the app.locals
   let { cardList } = app.locals;
 
+//parses the id
   id = parseInt(id);
+//card found originally starts as false
   let cardFound = false;
+//all cards are mapped over and if a card is found then cardfound is changed to true and the new card is returned
   const updatedCards = cardList.map(card => {
     if ( card.id == request.params.id) {
       cardFound = true;
@@ -107,11 +128,16 @@ app.put('/api/v1/cardList/:id', (request, res) => {
     }
   });
 
+//if there isnt a title or a content then an error is sent 
   if (!title || !content ) return response.status(422).json('Missing a title or content ');
+//if cardFound stays false then an error is sent
   if (!cardFound) return response.status(404).json('card was not found')
 
+//cardList is redeclared as all the updated cards
   app.locals.cardList = updatedCards;
+//if no errors were made it returns a 204 status
   return res.sendStatus(204)
 })
 
+//exports app file
 module.exports= app 
